b1e26f0c7037abe4808568f7f191ee58f2d67f3c
owokomoses <owokomose@gmail.com>
2024-09-06 00:58:48 +0300

    added navigation.kt and in it added loginpage in the navhost

---
+++ app/src/main/java/com/example/alumnimanagementsystemapp/Navigation.kt
@@ -0,0 +1,20 @@
+package com.example.alumnimanagementsystemapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.alumnimanagementsystemapp.pages.LoginPage
+
+@Composable
+fun Navigation(modifier: Modifier = Modifier){
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "login", builder = {
+        composable("login"){
+            LoginPage(modifier)
+        }
+    })
+
+}
