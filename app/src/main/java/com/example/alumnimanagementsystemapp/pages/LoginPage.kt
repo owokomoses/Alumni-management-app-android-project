666a8dbe8dace4472dabe29bf6337c933a9c984b
owokomoses <owokomose@gmail.com>
2024-09-06 00:45:29 +0300

    my first commit

---
+++ app/src/main/java/com/example/alumnimanagementsystemapp/pages/LoginPage.kt
@@ -0,0 +1,9 @@
+package com.example.alumnimanagementsystemapp.pages
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoginPage(modifier: Modifier = Modifier){
+
+}
