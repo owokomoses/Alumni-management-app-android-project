146562de4162e0179dd76124acc44b8edbb7f775
owokomoses <owokomose@gmail.com>
2024-09-10 01:29:46 +0300

    added welcomescreen in navhost

--- app/src/main/java/com/example/alumnimanagementsystemapp/Navigation.kt
+++ app/src/main/java/com/example/alumnimanagementsystemapp/Navigation.kt
@@ -8,18 +8,25 @@ import androidx.navigation.compose.rememberNavController
 import com.example.alumnimanagementsystemapp.pages.HomePage
 import com.example.alumnimanagementsystemapp.pages.LoginPage
 import com.example.alumnimanagementsystemapp.pages.SignupPage
+import com.example.alumnimanagementsystemapp.screens.WelcomeScreen
 
 @Composable
 fun Navigation(modifier: Modifier = Modifier,authViewModel: AuthViewModel){
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "login", builder = {
+    NavHost(navController = navController, startDestination = "welcome", builder = {
+
+        composable("welcome") { WelcomeScreen(navController = navController)
+        }
+
         composable("login"){
             LoginPage(modifier,navController,authViewModel)
         }
+
         composable("signup"){
             SignupPage(modifier,navController,authViewModel)
         }
+
         composable("home"){
             HomePage(modifier,navController,authViewModel)
         }
