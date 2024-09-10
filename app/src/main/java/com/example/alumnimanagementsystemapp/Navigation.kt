1acb826ce057f35577b31d0d658504c661be85a8
owokomoses <owokomose@gmail.com>
2024-09-11 01:38:25 +0300

    added launcheffect on navigation

--- app/src/main/java/com/example/alumnimanagementsystemapp/Navigation.kt
+++ app/src/main/java/com/example/alumnimanagementsystemapp/Navigation.kt
@@ -1,6 +1,8 @@
 package com.example.alumnimanagementsystemapp
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -14,6 +16,31 @@ import com.example.alumnimanagementsystemapp.screens.WelcomeScreen
 @Composable
 fun Navigation(modifier: Modifier = Modifier,authViewModel: AuthViewModel){
     val navController = rememberNavController()
+    val authState = authViewModel.authState.observeAsState()
+
+    // Check the user's authentication state at the start
+    LaunchedEffect(authState.value) {
+        when (authState.value) {
+            is AuthState.Authenticated -> {
+                // Navigate to Home if already authenticated
+                navController.navigate("home") {
+                    popUpTo("welcome") { inclusive = true }
+                }
+            }
+            is AuthState.Unauthenticated -> {
+                // If unauthenticated, navigate to loginScreen
+                navController.navigate("loginScreen") {
+                    popUpTo("welcome") { inclusive = true }
+                }
+            }
+            else -> {
+                // Show Welcome Screen by default
+                navController.navigate("welcome") {
+                    popUpTo("welcome") { inclusive = true }
+                }
+            }
+        }
+    }
 
     NavHost(navController = navController, startDestination = "welcome", builder = {
 
