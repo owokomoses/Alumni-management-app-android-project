336e331b9599c228fdf7954d021c3596350f9ca9
owokomoses <owokomose@gmail.com>
2024-09-10 03:26:15 +0300

    added image and background color in login

--- app/src/main/java/com/example/alumnimanagementsystemapp/pages/LoginPage.kt
+++ app/src/main/java/com/example/alumnimanagementsystemapp/pages/LoginPage.kt
@@ -1,11 +1,14 @@
 package com.example.alumnimanagementsystemapp.pages
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -19,12 +22,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.alumnimanagementsystemapp.AuthState
 import com.example.alumnimanagementsystemapp.AuthViewModel
+import com.example.alumnimanagementsystemapp.R
 
 @Composable
 fun LoginPage(modifier: Modifier = Modifier,navController: NavController,authViewModel: AuthViewModel){
@@ -50,10 +56,18 @@ fun LoginPage(modifier: Modifier = Modifier,navController: NavController,authVie
     }
 
     Column(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier.fillMaxSize().background(Color.White),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ){
+        Image(
+            painter = painterResource(id = R.drawable.icon), // Replace with your image resource
+            contentDescription = "Login Icon",
+            modifier = Modifier.size(100.dp) // Adjust the size as necessary
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
         Text(text = "Login Page", fontSize = 32.sp)
         
         Spacer(modifier = Modifier.height(16.dp))
