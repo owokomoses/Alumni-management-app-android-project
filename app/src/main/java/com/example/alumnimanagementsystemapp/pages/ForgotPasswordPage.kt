b19097af9e546bf3d41f0f2938b3159a9c4869ed
owokomoses <owokomose@gmail.com>
2024-09-16 02:08:02 +0300

    added forgot password page

---
+++ app/src/main/java/com/example/alumnimanagementsystemapp/pages/ForgotPasswordPage.kt
@@ -0,0 +1,171 @@
+package com.example.alumnimanagementsystemapp.pages
+
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.alumnimanagementsystemapp.AuthState
+import com.example.alumnimanagementsystemapp.AuthViewModel
+import com.example.alumnimanagementsystemapp.R
+
+@Composable
+fun ForgotPasswordPage(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+    authViewModel: AuthViewModel
+) {
+    var password by remember { mutableStateOf("") }
+
+    var confirmPassword by remember { mutableStateOf("") }
+
+    var confirmPasswordVisible by remember { mutableStateOf(false) }
+
+    var passwordVisible by remember {
+        mutableStateOf(false)
+    }
+
+    var emailIsFocused by remember { mutableStateOf(false) }
+
+    val focusRequester = remember { FocusRequester() }
+
+    val authState = authViewModel.authState.observeAsState()
+    val context = LocalContext.current
+
+    LaunchedEffect(authState.value) {
+        when (authState.value) {
+            is AuthState.Authenticated -> navController.navigate("login"){
+                // Remove SignupPage from the back stack
+                popUpTo("signup") { inclusive = true }
+            }
+            is AuthState.Error -> Toast.makeText(
+                context,
+                (authState.value as AuthState.Error).message, Toast.LENGTH_SHORT
+            ).show()
+
+            else -> Unit
+        }
+    }
+
+    Column(
+        modifier = modifier.fillMaxSize().background(Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.icon), // Replace with your image resource
+            contentDescription = "Login Icon",
+            modifier = Modifier.size(100.dp) // Adjust the size as necessary
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "Reset Password", fontSize = 32.sp, color = Color.Red)
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = {
+                Text(
+                    text = "Password",
+                    color = if (emailIsFocused) Color.Red else Color.Gray
+                )
+            },
+            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                IconButton(onClick = {
+                    passwordVisible = !passwordVisible
+                }) {
+                    Icon(
+                        imageVector = if (passwordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff,
+                        contentDescription = if (passwordVisible) "Hide password" else "Show password",
+                        tint = Color.Red
+                    )
+                }
+            },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Color.Red,
+                unfocusedBorderColor = Color.Gray,
+                cursorColor = Color.Red
+            ),
+            modifier = Modifier
+                .focusRequester(focusRequester)
+                .onFocusChanged { focusState ->
+                    emailIsFocused = focusState.isFocused
+                },
+            textStyle = TextStyle(color = Color.Gray)
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = confirmPassword,
+            onValueChange = { confirmPassword = it },
+            label = {
+                Text(
+                    text = "Confirm Password",
+                    color = if (emailIsFocused) Color.Red else Color.Gray
+                )
+            },
+            visualTransformation = if (confirmPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                IconButton(onClick = {
+                    confirmPasswordVisible = !confirmPasswordVisible
+                }) {
+                    Icon(
+                        imageVector = if (confirmPasswordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff,
+                        contentDescription = if (confirmPasswordVisible) "Hide password" else "Show password",
+                        tint = Color.Red
+                    )
+                }
+            },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Color.Red,
+                unfocusedBorderColor = Color.Gray,
+                cursorColor = Color.Red
+            ),
+            modifier = Modifier
+                .focusRequester(focusRequester)
+                .onFocusChanged { focusState ->
+                    emailIsFocused = focusState.isFocused
+                },
+            textStyle = TextStyle(color = Color.Gray)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+
+        },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
+        ) {
+            Text(text = "Reset password", color = Color.Black)
+        }
+
+
+    }
+}
