995d7a2d76dc310683fe46bcc2ef333140db8306
owokomoses <owokomose@gmail.com>
2024-09-17 23:29:49 +0300

    added bottomnavigationitems

--- app/src/main/java/com/example/alumnimanagementsystemapp/screens/MainScreen.kt
+++ app/src/main/java/com/example/alumnimanagementsystemapp/screens/MainScreen.kt
@@ -1,12 +1,20 @@
 package com.example.alumnimanagementsystemapp.screens
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 
@@ -16,9 +24,39 @@ import androidx.compose.ui.tooling.preview.Preview
 @Composable
 fun MainScreen (){
 
+    var currentRoute by remember {
+        mutableStateOf("home")
+    }
+
+    val items = listOf(
+        BottomNavigationItem(
+            "Home",
+            "home",
+            Icons.Filled.Home
+        ),
+        BottomNavigationItem(
+            "Task",
+            "task",
+            Icons.Filled.Star
+        ),
+        BottomNavigationItem(
+            "Notification",
+            "notification",
+            Icons.Filled.Notifications
+        ),
+        BottomNavigationItem(
+            "Profile",
+            "profile",
+            Icons.Filled.AccountCircle
+        )
+
+    )
+
     Scaffold (
         bottomBar = {
-            
+            BottomNavigationBar(items= ,currentScreen = currentRoute ) {
+
+            }
         }
     ){ paddingValues ->
         ScreenOne(paddingValues)
