d035474c7284d147a511815af91a61f6f2eab84e
owokomoses <owokomose@gmail.com>
2024-09-25 00:13:14 +0300

    added the opening and closing fun for nav drawer

--- app/src/main/java/com/example/alumnimanagementsystemapp/MainActivity.kt
+++ app/src/main/java/com/example/alumnimanagementsystemapp/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
@@ -17,13 +18,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.AccountCircle
 import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -31,6 +36,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -39,6 +45,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.alumnimanagementsystemapp.ui.theme.AlumniManagementSystemAppTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -66,17 +73,41 @@ fun Screen(modifier: Modifier = Modifier){
         state = rememberTopAppBarState()
     )
 
-    Scaffold(
-        modifier = modifier
-            .nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = {
-            TopBar(
-                scrollBehavior = scrollBehavior)
+    val drawerState = rememberDrawerState(
+        initialValue = DrawerValue.Closed
+    )
+
+    val scope = rememberCoroutineScope()
+    
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                DrawerContent()
+            }
         }
-    ) { paddingValues ->
-        ScreenContent(
-            paddingValues = paddingValues
-        )
+    ) {
+
+        Scaffold(
+            modifier = modifier
+                .nestedScroll(scrollBehavior.nestedScrollConnection),
+            topBar = {
+                TopBar(
+                    onOpenDrawer = {
+                        scope.launch {
+                            drawerState.apply{
+                                if (isClosed) open() else close()
+                            }
+                        }
+                    },
+                    scrollBehavior = scrollBehavior
+                )
+            }
+        ) { paddingValues ->
+            ScreenContent(
+                paddingValues = paddingValues
+            )
+        }
     }
 }
 
@@ -98,7 +129,7 @@ fun ScreenContent(paddingValues: PaddingValues){
                     .clip(RoundedCornerShape(20.dp))
                     .background(MaterialTheme.colorScheme.inversePrimary)
             )
-            
+
             Spacer(modifier = Modifier.height(16.dp))
         }
 
@@ -109,6 +140,7 @@ fun ScreenContent(paddingValues: PaddingValues){
 @Composable
 fun TopBar(
     modifier: Modifier = Modifier,
+    onOpenDrawer: () -> Unit,
     scrollBehavior: TopAppBarScrollBehavior
 ){
     TopAppBar(
@@ -130,7 +162,9 @@ fun TopBar(
             Icon(
                 imageVector = Icons.Rounded.Menu,
                 contentDescription = null,
-                modifier = Modifier
+                modifier = Modifier.clickable {
+                    onOpenDrawer()
+                }
                     .padding(start = 16.dp, end = 8.dp)
                     .size(27.dp)
             )
@@ -153,6 +187,9 @@ fun TopBar(
             )
         }
     )
-
 }
 
+@Composable
+fun DrawerContent(modifier: Modifier = Modifier){
+
+}
