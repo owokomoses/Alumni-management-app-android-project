fe3f3ad62e69f827d55bf0563e008079ef6ce8d3
owokomoses <owokomose@gmail.com>
2024-09-24 04:02:46 +0300

    added actions for topbar

--- app/src/main/java/com/example/alumnimanagementsystemapp/MainActivity.kt
+++ app/src/main/java/com/example/alumnimanagementsystemapp/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Menu
@@ -21,6 +22,7 @@ import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.alumnimanagementsystemapp.ui.theme.AlumniManagementSystemAppTheme
 
@@ -78,7 +80,19 @@ fun TopBar(
         navigationIcon = {
             Icon(
                 imageVector = Icons.Rounded.Menu,
-                contentDescription = null
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(start = 16.dp, end = 8.dp)
+                    .size(27.dp)
+            )
+        },
+        actions = {
+            Icon(
+                imageVector = Icons.Rounded.Menu,
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(start = 16.dp, end = 8.dp)
+                    .size(27.dp)
             )
         }
     )
