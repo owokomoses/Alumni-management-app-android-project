d07eb3f5501dcd680885738ad0ecea9a32b649ea
owokomoses <owokomose@gmail.com>
2024-09-25 04:25:43 +0300

    made changes in nav drawer width

--- app/src/main/java/com/example/alumnimanagementsystemapp/MainActivity.kt
+++ app/src/main/java/com/example/alumnimanagementsystemapp/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DrawerValue
@@ -89,8 +90,12 @@ fun Screen(modifier: Modifier = Modifier,navController: NavController, authViewM
     ModalNavigationDrawer(
         drawerState = drawerState,
         drawerContent = {
-            ModalDrawerSheet {
-                DrawerContent()
+            Box(
+                modifier = Modifier.width(240.dp) // Set the desired width here
+            ) {
+                ModalDrawerSheet {
+                    DrawerContent()
+                }
             }
         }
     ) {
